472aada5ca5ffebaa5c03ff35c75691299bcb72e
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-02 13:18:56 -0700

    styleing

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,11 @@ CREATE DATABASE dive_db
 USE DATABASE dive_db
 
 
-CREATE TABLE top_spots(
+CREATE TABLE topSpots(
 	id INT NOT NULL AUTO_INCREMENT,
 	spot varchar(255) NOT NULL,
 	images varchar(500) NOT NULL,
 	body varchar(1000) NOT NULL,
+	zip INTEGER(5) NOT NULL
 	PRIMARY KEY (id)
 );
