080a8f8968ca2d9c6fa0e0ae81bf05ba61f609fd
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-03 10:50:49 -0700

    got weather condition working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -81,22 +81,23 @@ module.exports = function (app) {
                 var hbsObject = {
                     topSpots: spotObj
                 };
-                console.log("line 24" + hbsObject);
+                // console.log("line 24" + hbsObject);
 
                 var apiKey = "appid=9b645d08bdee68ac9c3cf483c0d243d5";
                 var zip = data.zip;
                 var weatherQuery = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&" + apiKey;
-                var weatherObj = {};
                 request(weatherQuery, function (error, response, body) {
                     if (!error && response.statusCode === 200) {
 
-                        spotObj.desc = JSON.parse(body).weather.description;
+                        spotObj.desc = JSON.parse(body).weather[0].main;
+                        // console.log("line 94" + body).coord.weather.main;
                         spotObj.temp = JSON.parse(body).main.temp;
                         spotObj.maxTemp = JSON.parse(body).main.temp_max;
                         spotObj.minTemp = JSON.parse(body).main.temp_min;
 
 
-                        // console.log("line 36" , JSON.parse(body));
+                        console.log("line 36" , JSON.parse(body));
+                        console.log("line 36" , JSON.parse(body));
                         // console.log(spotObj);
                         res.render("index", spotObj);
                     }
