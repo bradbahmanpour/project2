cf5cb86316dfe95fc776fbceea752387b8bcad0c
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-04 09:57:36 -0700

    fixed class model

--- models/classes.js
+++ models/classes.js
@@ -1,7 +1,6 @@
-var express = require('sequelize');
+var express = require("sequelize");
 
 module.exports = function (sequelize, DataTypes) {
-<<<<<<< HEAD
     // Creates a "Character" model that matches up with DB
     var classes = sequelize.define("classes", {
         // the name of the character (a string)
@@ -47,50 +46,3 @@ module.exports = function (sequelize, DataTypes) {
     });
     return classes;
 };
-=======
-   // Creates a “Character” model that matches up with DB
-   var classes = sequelize.define('classes', {
-       // the name of the character (a string)
-       className: {
-           type: DataTypes.STRING,
-           allowNull: false,
-           validate: {
-               len: [1]
-           }
-       },
-       classDescription: {
-           type: DataTypes.STRING,
-           allowNull: false,
-           validate: {
-               len: [1]
-           }
-       },
-       classSchedule: {
-           type: DataTypes.STRING,
-           allowNull: false,
-           validate: {
-               len: [1]
-           }
-       },
-       classPrerequisit: {
-           type: DataTypes.STRING,
-           allowNull: false,
-           validate: {
-               len: [1]
-           }
-       },
-       classPrice: {
-           type: DataTypes.INTEGER
-       },
-   }, 
-   {
-       timestamps: false
-   }, {
-       indexes: [{
-           unique: true,
-           fields: ["className"]
-       }]
-   });
-   return classes;
-};
->>>>>>> DBlink
