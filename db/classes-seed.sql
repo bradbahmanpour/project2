16631f07f04d881d0af2540b0670e32156165cf2
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-03 20:19:21 -0700

    added seeds for classes

---
+++ db/classes-seed.sql
@@ -0,0 +1,10 @@
+
+DESCRIBE classes
+DROP TABLE classes
+SELECT * FROM classes
+
+INSERT INTO classes (className, classDescription, classSchedule, classPrerequisit, classPrice) VALUES ("SCUBA", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ", "monday-friday", "SCUBA and some more shit", 300);
+
+INSERT INTO classes (className, classDescription, classSchedule, classPrerequisit, classPrice) VALUES ("SCUBA Diving", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ","monday-friday", "SCUBA and some more shit", 400);
+
+INSERT INTO classes (className, classDescription, classSchedule, classPrerequisit, classPrice) VALUES ("SCUBA Steve's SCUBA class", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ","monday-friday", "SCUBA and some more shit", 1300);
